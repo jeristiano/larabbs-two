895e440cea50d340eda23938bcf06a6b75d52eb0
jeristiano <jeremykuang@foxmail.com>
2018-06-06 13:35:12 +0000

    list topics created by user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,9 +15,15 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Models\User::class, function (Faker $faker) {
     static $password;
+    // 随机取一个月以内的时间
+    $updated_at = $faker->dateTimeThisMonth();
+    // 传参为生成最大时间不超过，创建时间永远比更改时间要早
+    $created_at = $faker->dateTimeThisMonth($updated_at);
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
+        'created_at'=>$created_at,
+        'updated_at'=>$updated_at,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
     ];
