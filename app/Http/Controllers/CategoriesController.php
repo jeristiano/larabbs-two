6a203eac8f1ef109bc506e9a1113a69af453a9de
jeristiano <jeremykuang@foxmail.com>
2018-06-07 13:04:45 +0000

    user can create topics

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -12,8 +12,7 @@ class CategoriesController extends Controller
     public function show(Category $category,Request $request,Topic $topic)
     {
         //读取分类,每条按20个分页
-        $topics=$topic->with('user', 'category')
-            ->withOrder($request->order)
+        $topics=$topic->withOrder($request->order)
             ->where('category_id',$category->id)
             ->paginate(20);
         return view('topics.index',compact('topics','category'));
