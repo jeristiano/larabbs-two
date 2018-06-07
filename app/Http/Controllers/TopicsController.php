1b016ee568fadfa008164500ae1a697af1b11410
jeristiano <jeremykuang@foxmail.com>
2018-06-07 13:31:22 +0000

    user can upload topic image

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Handlers\ImageUploadHandler;
 use App\Models\Category;
 use App\Models\Topic;
 use Illuminate\Http\Request;
@@ -19,7 +20,7 @@ class TopicsController extends Controller
     public function index()
     {
         $topics = Topic::with('user', 'category')->paginate(30);
-        
+
         return view('topics.index', compact('topics'));
     }
 
@@ -29,39 +30,62 @@ class TopicsController extends Controller
     }
 
 
-	public function create(Topic $topic)
-	{
-	    $categories=Category::all();
-		return view('topics.create_and_edit', compact('topic', 'categories'));
-	}
+    public function create(Topic $topic)
+    {
+        $categories = Category::all();
+        return view('topics.create_and_edit', compact('topic', 'categories'));
+    }
 
-	public function store(TopicRequest $request,Topic $topic)
-	{
-	    $topic->fill($request->all());
-        $topic->user_id=Auth::id();
-		$topic->save();
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
-	}
+    public function store(TopicRequest $request, Topic $topic)
+    {
+        $topic->fill($request->all());
+        $topic->user_id = Auth::id();
+        $topic->save();
+        return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
+    }
 
-	public function edit(Topic $topic)
-	{
+    public function edit(Topic $topic)
+    {
         $this->authorize('update', $topic);
-		return view('topics.create_and_edit', compact('topic'));
-	}
+        return view('topics.create_and_edit', compact('topic'));
+    }
 
-	public function update(TopicRequest $request, Topic $topic)
-	{
-		$this->authorize('update', $topic);
-		$topic->update($request->all());
+    public function update(TopicRequest $request, Topic $topic)
+    {
+        $this->authorize('update', $topic);
+        $topic->update($request->all());
 
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
-	}
+        return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
+    }
 
-	public function destroy(Topic $topic)
-	{
-		$this->authorize('destroy', $topic);
-		$topic->delete();
+    public function destroy(Topic $topic)
+    {
+        $this->authorize('destroy', $topic);
+        $topic->delete();
 
-		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
-	}
+        return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
+    }
+
+    public function uploadImage(Request $request, ImageUploadHandler $uploader)
+    {
+        // 初始化返回数据，默认是失败的
+        $data = [
+            'success' => false,
+            'msg' => '上传失败!',
+            'file_path' => ''
+        ];
+
+        if ($file = $request->upload_file) {
+            $result = $uploader->save($request->upload_file, 'topics', Auth::id(), 1024);
+            // 图片保存成功的话
+            if ($result) {
+                $data['file_path'] = $result['path'];
+                $data['msg']       = "上传成功!";
+                $data['success']   = true;
+            }
+        }
+
+        return $data;
+
+    }
 }
