1d94bd8ec3ed688feaf7dc193bd360752b143659
jeristiano <jeremykuang@foxmail.com>
2018-06-12 09:19:29 +0000

    yser can delete reply

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -13,7 +13,7 @@ class ReplyObserver
     public function created(Reply $reply)
     {
 
-        $topic=$reply->topic;
+        $topic = $reply->topic;
         $reply->topic->increment('reply_count', 1);
         // 通知作者话题被回复了
         $topic->user->notify(new TopicReplied($reply));
@@ -23,6 +23,12 @@ class ReplyObserver
     public function creating(Reply $reply)
     {
         $reply->content = clean($reply->content, 'user_topic_body');
+    }
+
+    public function deleted(Reply $reply)
+    {
+        $reply->topic->decrement('reply_count', 1);
+
     }
 
 
--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -4,6 +4,7 @@ namespace App\Observers;
 
 use App\Handlers\SlugTranslateHandler;
 use App\Jobs\TranslateSlug;
+use App\Models\Reply;
 use App\Models\Topic;
 
 // creating, created, updating, updated, saving,
@@ -20,15 +21,23 @@ class TopicObserver
 
 
     }
-    public function saved(Topic $topic){
+
+    public function saved(Topic $topic)
+    {
         // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
         // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
-        if ( ! $topic->slug) {
+        if (!$topic->slug) {
             // 推送任务到队列
             dispatch(new TranslateSlug($topic));
         }
 
-}
+    }
+
+    public function deleted(Topic $topic)
+    {
+        \DB::table('replies')->where('topic_id', $topic->id)->delete();
+
+    }
 
 
 }
