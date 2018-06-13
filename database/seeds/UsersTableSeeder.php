e26f75f9d3763e42d162e6e32bd898a4a210b78c
jeristiano <jeremykuang@foxmail.com>
2018-06-13 10:02:55 +0000

    add manage contents permission and visit horizion

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -37,15 +37,17 @@ class UsersTableSeeder extends Seeder
         \App\Models\User::insert($user_array);
         // 单独处理第一个用户的数据
         $user = \App\Models\User::find(1);
+        // 初始化用户角色，将 1 号用户指派为『站长』
+        $user->assignRole('Founder');
         $user->name = 'jeremy';
         $user->email = 'jeremykuang@qq.com';
         $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200';
-        // 初始化用户角色，将 1 号用户指派为『站长』
-        $user->assignRole('Founder');
+
+        $user->save();
+
         // 将 2 号用户指派为『管理员』
         $user = \App\Models\User::find(2);
         $user->assignRole('Maintainer');
-        $user->save();
 
     }
 }
